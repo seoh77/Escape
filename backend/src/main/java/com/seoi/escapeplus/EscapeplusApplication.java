497ceac98992121527bc57bd04d55085e8d445a5
seoh77 <seoh7dev@gmail.com>
2026-01-05 20:54:03 +0900

    init: 백엔드 초기 세팅

---
+++ backend/src/main/java/com/seoi/escapeplus/EscapeplusApplication.java
@@ -0,0 +1,13 @@
+package com.seoi.escapeplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscapeplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscapeplusApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/seoi/escapeplus/EscapeplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoi.escapeplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapeplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
